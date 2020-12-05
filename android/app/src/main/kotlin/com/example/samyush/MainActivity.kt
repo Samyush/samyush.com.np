0d3964ff2ef0791a486757effa1924d4a43d535f
Samyush <samyushmaharjan@gmail.com>
2020-12-05 21:30:39 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/samyush/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.samyush
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Components/default_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+
+class DefaultButton extends StatelessWidget {
+  final Icon icons;
+  final Function press;
+  const DefaultButton({
+    Key key,
+    this.icons,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: FlatButton(
+        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+        color: kPrimaryColor,
+        onPressed: press,
+        child: icons,
+      ),
+    );
+  }
+}
