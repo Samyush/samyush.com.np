dc75dd51914d45aed50d5857c1c70014c25bbfb9
Samyush <samyushmaharjan@gmail.com>
2021-02-07 21:56:05 +0545

    correcting linkedin link

--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -15,7 +15,7 @@ class HomeScreen extends StatelessWidget {
         width: size.width,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("assets/images/bg.png"),
+            image: AssetImage("assets/images/books.jpg"),
             fit: BoxFit.cover,
           ),
         ),
