2d01447010c219c59d675b214ef66f83e9d870eb
Samyush <samyushmaharjan@gmail.com>
2021-02-07 20:12:15 +0545

    correcting linkedin link

--- lib/Screens/Home/Components/body.dart
+++ lib/Screens/Home/Components/body.dart
@@ -41,7 +41,7 @@ class _BodyState extends State<Body> {
                 ),
           ),
           Text(
-            "Too much in love with programming, and \nReally love good foods \nyou can look up on me by samyush on github too",
+            "We tech coding and math for free, and \nA small personal institute to help all in need \nyou can look up on me by samyush on github too",
             style: TextStyle(
               fontSize: 21,
               color: kTextcolor.withOpacity(0.34),
